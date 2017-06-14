1de304bd43919c5e7b7af01593ac7a8153bd51f6
Nick Bohlen <mail@bohlen-design.de>
2017-06-14 09:52:31 +0200

    mobile

--- js/ep-script.js
+++ js/ep-script.js
@@ -20,7 +20,24 @@ $(document).ready(function () {
     paginationClickable: true,
     spaceBetween: 30,
     grabCursor: true,
-    autoplayDisableOnInteraction: true
+    autoplayDisableOnInteraction: true,
+    breakpoints: {
+        // when window width is <= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10
+        },
+        // when window width is <= 480px
+        480: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is <= 640px
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        }
+      }
     });
       
   $('.navbar').offcanvas({
