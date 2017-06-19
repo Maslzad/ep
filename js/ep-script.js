00ec456d956599ff3d8cc5a0bf736461e7b9d749
Nick Bohlen <mail@bohlen-design.de>
2017-06-19 17:07:39 +0200

    mobile opt

--- js/ep-script.js
+++ js/ep-script.js
@@ -1,5 +1,36 @@
+/**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ */
+function detectIE() {
+    var ua = window.navigator.userAgent;
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+       // Edge (IE 12+) => return version number
+       return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+}
+
 $(document).ready(function () {
-  var swiper = new Swiper('.swiper-container', {
+  if( detectIE() ){ // MSIE Browser
+    var swiper = new Swiper('.swiper-container', {
           direction: 'vertical',
           pagination: '.swiper-pagination',//
 //           effect: 'cube',
@@ -13,6 +44,22 @@ $(document).ready(function () {
 //           slideShadows: false
 //           }
       });
+    }else{ // other Browser
+      var swiper = new Swiper('.swiper-container', {
+            direction: 'vertical',
+            pagination: '.swiper-pagination',//
+            effect: 'cube',
+            grabCursor: false,
+            autoplay: 10000,
+            keyboardControl: true,
+            mousewheelControl: true,
+            loop: true,
+            cube: {
+            shadow: false,
+            slideShadows: false
+            }
+        });
+    }
   
   var team_swiper = new Swiper('.team-swiper-container', {
     pagination: '.swiper-pagination',
