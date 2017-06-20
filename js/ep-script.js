343a0edb3ba8edfc569018420689d31847c7913a
Nick Bohlen <mail@bohlen-design.de>
2017-06-20 12:05:24 +0200

    fix

--- js/ep-script.js
+++ js/ep-script.js
@@ -71,18 +71,18 @@ $(document).ready(function () {
     breakpoints: {
         // when window width is <= 320px
         320: {
-          slidesPerView: 1,
-          spaceBetween: 10
+          slidesPerView: 2,
+          spaceBetween: 20
         },
         // when window width is <= 480px
         480: {
-          slidesPerView: 1,
+          slidesPerView: 2,
           spaceBetween: 20
         },
         // when window width is <= 640px
-        640: {
-          slidesPerView: 3,
-          spaceBetween: 30
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 20
         }
       }
     });
